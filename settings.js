d2b5e3f8bf58616930f6d6d9fb69c6ef9e0bb1ff
keenzx] <botkeenan@gmail.com>
2024-10-14 21:45:43 +0700

    local

---
+++ settings.js
@@ -0,0 +1,12 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['62882000444707'] //OWNER
+global.botname = 'ZuraBotz' //BOT NAME
+global.versisc = "5.0.0" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "AstraaLuckIsreal" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "ZuraBotz" //OPSIONAL
+global.simbol = "" // SIMBOL MENU
+global.idCH = null;
