087b507a7bef41180cd0cfef962af808f822377d
astraa9012 <astraa9012@gmail.com>
2024-10-15 19:57:20 +0700

    Update five.js

--- five.js
+++ five.js
@@ -1,7 +1,7 @@
 require('./settings')
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware untuk parsing JSON
 app.use(express.json());
